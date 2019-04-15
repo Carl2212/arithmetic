ae9f6dfe9559a112168980084171f1a5ba9188b0
forbetter100 <ainianyu854362212@163.com>
2019-04-16 00:44:20 +0800

    leetcode:reverse digits of an integer

--- reverseDigits.js
+++ reverseDigits.js
@@ -1,5 +1,21 @@
 
+/**
+** Given a 32-bit signed integer, reverse digits of an integer.
+Example 1:
+  Input: 123
+  Output: 321
+Example 2:
+  Input: -123
+  Output: -321
+Example 3:
+  Input: 120
+  Output: 21
 
+ INT range: -2147483648到2147483647
+ INT_MAX  2147483647
+ INT_MIN -2147483648
+**
+**/
 function reverseDigits(num) {
 	let rev = 0;
 	while(num > 0){
